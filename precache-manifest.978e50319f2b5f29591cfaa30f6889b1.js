2039f821406448ed875d0b65a8845b3e0aa0c455
Kalita Mikhail <mkalita1991@gmail.com>
2019-06-24 05:09:18 +0300

    Updates

---
+++ precache-manifest.978e50319f2b5f29591cfaa30f6889b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29e53632a21b75d69d5705cd0d7e2635",
+    "url": "/notes-task/index.html"
+  },
+  {
+    "revision": "b033d9d31f48ab02895f",
+    "url": "/notes-task/static/css/main.579d6a42.chunk.css"
+  },
+  {
+    "revision": "45d920a4f1b60f733be1",
+    "url": "/notes-task/static/js/2.60fa24d5.chunk.js"
+  },
+  {
+    "revision": "b033d9d31f48ab02895f",
+    "url": "/notes-task/static/js/main.7095ef0a.chunk.js"
+  },
+  {
+    "revision": "17ae79d65ad3a3a6b09f",
+    "url": "/notes-task/static/js/runtime~main.bb0d0582.js"
+  }
+]);
